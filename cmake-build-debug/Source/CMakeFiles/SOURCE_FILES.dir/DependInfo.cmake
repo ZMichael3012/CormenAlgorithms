0969feb2de5f3636af19f609268fdf021e3f7ff7
ZMichael <zaplatinme@gmail.com>
2020-04-21 02:44:34 +0700

    Added Insertion Sort in Sorts.cpp, TODO to main.cpp, 2 folders: Headers and Source -, and CMake files (one is in Source)

---
+++ cmake-build-debug/Source/CMakeFiles/SOURCE_FILES.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/MichaelZ/CLionProjects/CormenAlgorithms/Source/Sorts.cpp" "C:/Users/MichaelZ/CLionProjects/CormenAlgorithms/cmake-build-debug/Source/CMakeFiles/SOURCE_FILES.dir/Sorts.cpp.obj"
+  "C:/Users/MichaelZ/CLionProjects/CormenAlgorithms/Source/main.cpp" "C:/Users/MichaelZ/CLionProjects/CormenAlgorithms/cmake-build-debug/Source/CMakeFiles/SOURCE_FILES.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
